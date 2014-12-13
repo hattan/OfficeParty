7ae15de3adc15b7e7bfecd4ead6773e9e9481cef
Hattan Shobokshi <hattan@gmail.com>
2014-12-12 22:35:48 -0800

    removed config values

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-	mongoUrl : 'mongodb://hattan:test123@ds063240.mongolab.com:63240/officeparty',
+	mongoUrl : '',
 	youTube : {
-		key : 'AIzaSyDDWf_ETIKJ-X48u1IELy4wHw5O4bj244E',
+		key : '',
 		maxResults : 10		
 	},
 	facebook : {
-		clientID: '1576762302539344',
-		clientSecret : 'e23387548135f5393321e99e3d41b825',
-		callbackURL : 'http://localhost:3000/auth/facebook/callback'
+		clientID: '',
+		clientSecret : '',
+		callbackURL : ''
 	}
 }
